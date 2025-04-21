87d3b7ee5060936d8333290b371146d6df26a4e9
rodeka <rodeka.core@ya.ru>
2025-04-22 01:02:14 +0300

    add scrypt and tests for it

---
+++ src/crypto/scrypt.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include "pbkdf2.h"
+
+#define R 8
+#define SALSA_ROUNDS 8
+
+static inline uint32_t ROTL32(uint32_t v, unsigned c) {
+    return (v << c) | (v >> (32 - c));
+}
+
+void uint32_to_le(uint32_t v, uint8_t *p) {
+    p[0] = (uint8_t)(v);
+    p[1] = (uint8_t)(v >> 8);
+    p[2] = (uint8_t)(v >> 16);
+    p[3] = (uint8_t)(v >> 24);
+}
+
+static inline uint32_t le_to_uint32(const uint8_t *p) {
+    return (uint32_t)p[0] |
+           ((uint32_t)p[1] << 8) |
+           ((uint32_t)p[2] << 16) |
+           ((uint32_t)p[3] << 24);
+}
+
+void salsa20_8(uint8_t *out, const uint8_t *in)
+{
+    uint32_t x[16], orig[16];
+
+    for (int i = 0; i < 16; i++)
+        orig[i] = x[i] = le_to_uint32(in + 4 * i);
+
+    for (int i = 0; i < SALSA_ROUNDS; i += 2) {
+        // column round
+        x[ 4] ^= ROTL32(x[ 0] + x[12], 7);
+        x[ 8] ^= ROTL32(x[ 4] + x[ 0], 9);
+        x[12] ^= ROTL32(x[ 8] + x[ 4],13);
+        x[ 0] ^= ROTL32(x[12] + x[ 8],18);
+
+        x[ 9] ^= ROTL32(x[ 5] + x[ 1], 7);
+        x[13] ^= ROTL32(x[ 9] + x[ 5], 9);
+        x[ 1] ^= ROTL32(x[13] + x[ 9],13);
+        x[ 5] ^= ROTL32(x[ 1] + x[13],18);
+
+        x[14] ^= ROTL32(x[10] + x[ 6], 7);
+        x[ 2] ^= ROTL32(x[14] + x[10], 9);
+        x[ 6] ^= ROTL32(x[ 2] + x[14],13);
+        x[10] ^= ROTL32(x[ 6] + x[ 2],18);
+
+        x[ 3] ^= ROTL32(x[15] + x[11], 7);
+        x[ 7] ^= ROTL32(x[ 3] + x[15], 9);
+        x[11] ^= ROTL32(x[ 7] + x[ 3],13);
+        x[15] ^= ROTL32(x[11] + x[ 7],18);
+
+        // row round
+        x[ 1] ^= ROTL32(x[ 0] + x[ 3], 7);
+        x[ 2] ^= ROTL32(x[ 1] + x[ 0], 9);
+        x[ 3] ^= ROTL32(x[ 2] + x[ 1],13);
+        x[ 0] ^= ROTL32(x[ 3] + x[ 2],18);
+
+        x[ 6] ^= ROTL32(x[ 5] + x[ 4], 7);
+        x[ 7] ^= ROTL32(x[ 6] + x[ 5], 9);
+        x[ 4] ^= ROTL32(x[ 7] + x[ 6],13);
+        x[ 5] ^= ROTL32(x[ 4] + x[ 7],18);
+
+        x[11] ^= ROTL32(x[10] + x[ 9], 7);
+        x[ 8] ^= ROTL32(x[11] + x[10], 9);
+        x[ 9] ^= ROTL32(x[ 8] + x[11],13);
+        x[10] ^= ROTL32(x[ 9] + x[ 8],18);
+
+        x[12] ^= ROTL32(x[15] + x[14], 7);
+        x[13] ^= ROTL32(x[12] + x[15], 9);
+        x[14] ^= ROTL32(x[13] + x[12],13);
+        x[15] ^= ROTL32(x[14] + x[13],18);
+    }
+
+    for (int i = 0; i < 16; i++)
+        x[i] += orig[i];
+
+    for (int i = 0; i < 16; i++)
+        uint32_to_le(x[i], out + 4 * i);
+}
+
+
+void BlockMix(uint8_t *out, const uint8_t *in)
+{
+    uint8_t X[64];
+    memcpy(X, in + (2 * R - 1) * 64, 64);
+
+    for (int i = 0; i < 2 * R; i++) {
+        for (int j = 0; j < 64; j++)
+            X[j] ^= in[i * 64 + j];
+
+        salsa20_8(X, X);
+
+        if ((i & 1) == 0)
+            memcpy(out + (i / 2) * 64, X, 64);
+        else
+            memcpy(out + (R + i / 2) * 64, X, 64);
+    }
+}
+
+static inline uint64_t integerify(const uint8_t *X)
+{
+    const uint8_t *last64 = X + (128 * R - 64);
+    return (uint64_t)le_to_uint32(last64) | ((uint64_t)le_to_uint32(last64 + 4) << 32);
+}
+
+void ROMix(uint8_t *B, uint32_t N)
+{
+    const size_t blk = 128 * R;
+    uint8_t *V = malloc((size_t)N * blk);
+    uint8_t *X = malloc(blk);
+    uint8_t *Y = malloc(blk);
+
+    memcpy(X, B, blk);
+
+    for (uint32_t i = 0; i < N; i++) {
+        memcpy(V + (size_t)i * blk, X, blk);
+        BlockMix(Y, X);
+        memcpy(X, Y, blk);
+    }
+
+    for (uint32_t i = 0; i < N; i++) {
+        uint64_t j = integerify(X) & (N - 1);
+        uint8_t *Vj = V + j * blk;
+
+        for (size_t k = 0; k < blk; k++)
+            X[k] ^= Vj[k];
+
+        BlockMix(Y, X);
+        memcpy(X, Y, blk);
+    }
+
+    memcpy(B, X, blk);
+
+    free(Y);
+    free(X);
+    free(V);
+}
+
+
+void scrypt(const uint8_t *P, uint32_t plen, const uint8_t *S, uint32_t slen, uint32_t N, uint32_t r, uint32_t p, uint8_t *out, uint32_t dkLen)
+{
+    const size_t B_len = (size_t)p * 128 * r;
+    uint8_t *B = (uint8_t *)malloc(B_len);
+
+    pbkdf2_hmac_sha256(P, plen, S, slen, 1, B_len, B);
+
+    for (uint32_t i = 0; i < p; i++)
+        ROMix(B + (size_t)i * 128 * r, N);
+
+    pbkdf2_hmac_sha256(P, plen, B, B_len, 1, dkLen, out);
+
+    free(B);
+}
---
+++ tests/test_scrypt.c
@@ -0,0 +1,110 @@
+#include <check.h>
+#include <stdio.h>
+#include <openssl/evp.h>
+#include <openssl/kdf.h>
+#include "scrypt.h"
+
+START_TEST(test_scrypt_same_as_OpenSSL){
+    const unsigned char password[] = {'1', '2', '3', '4'};
+    const unsigned char salt[] = {0x29, 0xBE, 0xC0, 0xE9, 0x3E, 0x91, 0xAB, 0xB0};
+    uint32_t N = 8192;
+    uint32_t r = 8;
+    uint32_t p = 1;
+    uint32_t dkLen = 32;
+    uint8_t dk1[dkLen];
+    uint8_t dk2[dkLen];
+
+    scrypt((const unsigned char*)password, sizeof(password), (const unsigned char*)salt, sizeof(salt), N, r, p, dk1, sizeof(dk1));
+
+    EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_SCRYPT, NULL);
+    if (!pctx) {
+        fprintf(stderr, "EVP_PKEY_CTX_new_id failed\n");
+        ck_abort();
+    }
+    
+    if (EVP_PKEY_derive_init(pctx) <= 0) {
+        fprintf(stderr, "EVP_PKEY_derive_init failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    
+    if (EVP_PKEY_CTX_set1_pbe_pass(pctx, (const char*)password, sizeof(password)) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set1_pbe_pass failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    
+    if (EVP_PKEY_CTX_set1_scrypt_salt(pctx, salt, sizeof(salt)) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set1_scrypt_salt failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    
+    if (EVP_PKEY_CTX_set_scrypt_N(pctx, N) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set_scrypt_N failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    
+    if (EVP_PKEY_CTX_set_scrypt_r(pctx, r) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set_scrypt_r failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    
+    if (EVP_PKEY_CTX_set_scrypt_p(pctx, p) <= 0) {
+        fprintf(stderr, "EVP_PKEY_CTX_set_scrypt_p failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    
+    size_t outlen = dkLen;
+    if (EVP_PKEY_derive(pctx, dk2, &outlen) <= 0) {
+        fprintf(stderr, "EVP_PKEY_derive failed\n");
+        EVP_PKEY_CTX_free(pctx);
+        ck_abort();
+    }
+    EVP_PKEY_CTX_free(pctx);
+    ck_assert_mem_eq(dk1, dk2, dkLen);
+}
+
+
+START_TEST(test_scrypt_same_behaviour){
+    const unsigned char password[] = {'1', '2', '3', '4'};
+    const unsigned char salt[] = {0x29, 0xBE, 0xC0, 0xE9, 0x3E, 0x91, 0xAB, 0xB0};
+    uint32_t N = 16384;
+    uint32_t r = 8;
+    uint32_t p = 1;
+    uint8_t dk1[32];
+    uint8_t dk2[32];
+
+    scrypt((const unsigned char*)password, sizeof(password), (const unsigned char*)salt, sizeof(salt), N, r, p, dk1, sizeof(dk1));
+
+    scrypt((const unsigned char*)password, sizeof(password), (const unsigned char*)salt, sizeof(salt), N, r, p, dk2, sizeof(dk2));
+
+    ck_assert_mem_eq(dk1, dk2, sizeof(dk1));
+}
+
+Suite* pbkdf2_suite(void){
+    Suite* s = suite_create("SCRYPT");
+    TCase* tc_scrypt_same_behaviour = tcase_create("SCRYPT_same_behavior");
+    TCase* tc_scrypt_same_as_OpenSSL = tcase_create("SCRYPT_same_as_OpenSSL");
+    
+    tcase_add_test(tc_scrypt_same_as_OpenSSL, test_scrypt_same_as_OpenSSL);
+    tcase_add_test(tc_scrypt_same_behaviour, test_scrypt_same_behaviour);
+    suite_add_tcase(s, tc_scrypt_same_as_OpenSSL);
+    suite_add_tcase(s, tc_scrypt_same_behaviour);
+    return s;
+}
+
+int main(void){
+    int fail_count;
+    Suite* s = pbkdf2_suite();
+    SRunner* sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    fail_count = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (fail_count == 0) ? 0 : 1;
+}
